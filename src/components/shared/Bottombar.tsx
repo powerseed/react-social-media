66ff835ce4c9a49f9a3b8bc005cc04b85f1c5c18
Jiehao Luo <powerseed5044@gmail.com>
2023-12-13 21:01:15 -0700

    complelte bottom bar

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { sidebarLinks } from "@/constants";
+import { INavLink } from "@/types";
+import { Link, useLocation } from "react-router-dom"
 
 const Bottombar = () => {
+  const { pathname } = useLocation();
+
   return (
-    <div>bottombar</div>
+    <section className="bottom-bar">
+      {sidebarLinks.map((link: INavLink) => {
+        const isActive = pathname === link.route;
+
+        return (
+          <Link to={link.route} key={link.label} className={`flex-center flex-col gap-1 p-2 transition ${isActive && 'bg-primary-500 rounded-[10px]'}`}>
+            <img src={link.imgURL} alt={link.label} width={16} height={16} className={`${isActive && 'invert-white'}`} />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        )
+      })}
+    </section>
   )
 }
 
